a34fe75993de9faff2da460db8811ffd7d4c8ec3
mikeklimanek <mike.klimanek@gmail.com>
2024-04-30 03:03:33 +0200

    creating connection with DB

--- crypto.py
+++ crypto.py
@@ -1,9 +1,19 @@
 import requests
 from datetime import datetime
+import dotenv
 import pytz
+import os
+import libsql_experimental as libsql
+dotenv.load_dotenv()
+API_URL = os.getenv('API_URL')
+url = os.getenv("TURSO_DATABASE_URL")
+auth_token = os.getenv("TURSO_AUTH_TOKEN")
+
+conn = libsql.connect("coin-alert.db", sync_url=url, auth_token=auth_token)
+conn.sync()
 
 
-api_result = requests.get('https://api.alternative.me/v2/ticker/?limit=1')
+api_result = requests.get(API_URL + '?limit=1')
 
 
 api_result_json = api_result.json()
@@ -17,6 +27,8 @@ last_updated_utc = datetime.utcfromtimestamp(last_updated_unix)
 cet_timezone = pytz.timezone('CET')
 last_updated_cet = last_updated_utc.replace(tzinfo=pytz.utc).astimezone(cet_timezone)
 formatted_date = last_updated_cet.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+
 
 
 print(f"Name: {name}")
