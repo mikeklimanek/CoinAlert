3a1d5594ca7328e15a20131b6e4259068a2be6b5
mikeklimanek <mike.klimanek@gmail.com>
2024-05-01 03:51:25 +0200

    CI/CD push

--- periodical.py
+++ periodical.py
@@ -1,12 +1,6 @@
-import schedule
-import time
 import os
 from crypto import fetch_crypto_data, conn
 from database.crypto_db import process_and_store_data
-
-
-
-
 
 def job():
     api_url = os.getenv('API_URL') + '?limit=10'  
@@ -14,14 +8,5 @@ def job():
     if data:
         process_and_store_data(data, conn)
 
-schedule.every(4).hours.do(job)
-
-try:
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Shutting down the scheduler...")
-    # Optionally, here you can add conn.close() if it's supported and necessary.
-except Exception as e:
-    print(f"An error occurred: {e}")
+if __name__ == "__main__":
+    job()
