9b3fec5093e277a259a1c922d2c7fc65b9e55ce2
mikeklimanek <mike.klimanek@gmail.com>
2024-05-04 00:06:39 +0200

    working email notifications of price changes

--- analytics/changes_alert.py
+++ analytics/changes_alert.py
@@ -1,7 +1,6 @@
 from utils.auth import get_database_connection
 from utils.email_setup import send_email
 
-
 conn = get_database_connection()
 
 def all_price_changes(conn, symbol):
@@ -38,18 +37,26 @@ def all_price_changes(conn, symbol):
     last_price = prices[-1]
     total_change_percent = ((last_price - first_price) / first_price) * 100
 
-    message = f"<br>Total change for {symbol} from -24h till now === {total_change_percent:.2f}% from ${first_price} to ${last_price}"
-    significant_changes = []
+    message_lines = []
+    message_lines.append(f"Total change for {symbol} in last 24 hours === {total_change_percent:.2f}% from ${first_price} to ${last_price}")
 
-    if abs(total_change_percent) >= 2:
-        significant_changes.append(message)
+
+    hours = 24
+    for i in range(len(prices) - 1):
+        current_price = prices[i]
+        next_price = prices[i + 1]
+        change_percent = ((next_price - current_price) / current_price) * 100
+        if abs(change_percent) >= 4:                # settings for percentage change
+            message_lines.append(f"{symbol} Changed from -{hours}h to -{hours-4}h === {change_percent:.2f}% || from ${current_price} to ${next_price}")
+        hours -= 4
+
     
-    return None, significant_changes
+    return None, message_lines if message_lines else None
 
 def send_notification(changes):
     if changes:
         subject = "Significant Crypto Price Changes"
-        body = "Here are the significant changes:\n" + "\n".join(changes)
+        body = "<strong>Here are the significant changes:</strong><br><br>" + "<br><br>".join(changes)
         send_email(subject, body)
 
 significant_changes = []
@@ -58,7 +65,8 @@ for symbol in ['BTC', 'ETH', 'ADA', 'BNB', 'USDT', 'XRP', 'DOGE', 'DOT', 'SOL', 
     if error:
         print(error)
     if changes:
-        significant_changes.extend(changes)
+        
+        significant_changes.extend(['<br>'.join(changes)])
 
 if significant_changes:
     send_notification(significant_changes)
