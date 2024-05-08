a28ec62349ad8ee3c9ca613c9590333d9f53afdf
mikeklimanek <mike.klimanek@gmail.com>
2024-05-08 03:27:39 +0200

    turning off libsql handshake spam

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from dotenv import load_dotenv
+import logging
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
 from analytics.results_rsi import print_latest_rsi_report
 from analytics.stocks_rsi import update_rsi_for_all_symbols
 
